51adb4112d1664a36b159fcbca0aefea4134c779
Rachel <rachel.rolon@gmail.com>
2020-10-06 20:46:47 -0500

    Add project files.

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class Menu
+    {
+        public List<MenuItem> MenuItems { get; set; }
+        public DateTime Updated { get; }
+
+        public Menu(DateTime updated, List<MenuItem> menuItems)
+        {
+            Updated = updated;
+            MenuItems = menuItems;
+        }
+    }
+}
---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class MenuItem
+    {
+        private double Price { get; set; }
+        private string Description { get; set; }
+        private string Category { get; set; }
+        private bool IsNew { get; set; }
+
+        public MenuItem(double price, string description, string category, bool isNew)
+        {
+            Price = price;
+            Description = description;
+            Category = category;
+            IsNew = isNew;
+
+        }
+    }
+}
